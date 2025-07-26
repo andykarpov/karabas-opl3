0980fdd9a431b36ffbd7546e8fa8ae74ae6f9262
Andy Karpov <andykarpov@users.noreply.github.com>
2025-07-26 02:02:01 +0200

    Added cpld firmware

--- cpld/rtl/top.v
+++ cpld/rtl/top.v
@@ -1,32 +1,39 @@
+`default_nettype none
+
 module karabas_opl3(
-    input n_rst,
-    input clk28,
-
-    input [4:0] cfg,
-
-    input [9:0] a,
-    input n_iorq,
-    input n_m1,
-    output n_iorqge,
-
-    output n_ym_cs,
-    output ym_a[1:0],
-    output clk14,
-
-    input ym_smp[2:1],
-    input ym_data,
-    input ym_dclk,
-
-    output dac_bck,
-    output dac_lrck,
-    output dac_dat
+    // master clock
+    input wire clk28,
+	 // config bits
+    input wire [4:0] cfg,
+	 // bus signals
+    input wire n_rst,
+    input wire [9:0] a,
+    input wire n_iorq,
+    input wire n_m1,
+	 // ymf262-m control signals
+    output wire n_iorqge,
+    output wire n_ym_cs,
+    output wire [1:0] ym_a,
+    output wire clk14,
+	 // ymf262-m sound stream
+    input wire [1:0] ym_smp,
+    input wire ym_data,
+    input wire ym_dclk,
+    // output i2s dac stream
+    output wire dac_bck,
+    output reg dac_lrck,
+    output wire dac_dat,
+	 output wire dac_std,
+	 // debug leds
+	 output wire [1:0] led
 );
 
 // clock for ymf262-m (divided by 2)
-reg clk14;
+reg clk_div = 0;
 always @(posedge clk28) begin
-    clk14 <= ~clk14;
+    clk_div <= ~clk_div;
 end
+assign clk14 = clk_div;
 
 // port access (range 0xc4 ... 0xc7)
 wire port_cs = a[7:2] == 6'b110001;
@@ -40,36 +47,40 @@ assign ym_a[0] = a[0];
 assign ym_a[1] = a[1];
 
 // convert data stream for i2s from lsb-first to msb-first
-reg [2:1] prev_smp;
+reg [1:0] prev_smp;
 reg [31:0] data = 32'b0;
 reg [16:0] i2s_data;
 reg [4:0] cnt = 0;
 
 always @(posedge ym_dclk) begin
-    prev_smp[2:1] = ym_smp[2:1];
+    prev_smp <= ym_smp;
     // left channel capture
-    if (!prev_smp[1] & ym_smp[1]) begin
+    if (!prev_smp[0] & ym_smp[0]) begin
         cnt <= 5'b0; // start capture of the left channel
         data[31:16] <= {ym_data, 15'b0};
     end
-    else if (ym_smp[1]) begin
+    else if (ym_smp[0]) begin
         cnt <= cnt + 1; // continue capturing of the left channel
         data[31:16] <= {ym_data, data[31:17]};
     end
     // right channel capture
-    if (!prev_smp[2] & ym_smp[2]) begin
+    if (!prev_smp[1] & ym_smp[1]) begin
         cnt <= 16; // start capture of the right channel
         data[15:0] <= {ym_data, 15'b0};
-    end else if (ym_smp[2]) begin
+    end else if (ym_smp[1]) begin
         cnt <= cnt + 1; // continue capturing of the right channel
         data[15:0] <= {ym_data, data[15:1]};
     end
 end
 
+// debug led counter (samplerate 46875/65536=0.7 Hz)
+reg [15:0] ym_clk_cnt = 0;
+
 // i2s output (todo)
 always @(posedge ym_dclk) begin
     // right channel data[15:0] is ready
     if (cnt == 0) begin
+			ym_clk_cnt <= ym_clk_cnt + 1;
         i2s_data <= data[15:0];
         dac_lrck <= 1'b0;
     end
@@ -79,14 +90,17 @@ always @(posedge ym_dclk) begin
         dac_lrck <= 1'b1;
     end
     else begin
-        i2s_data <= {i2s_data[14:0], 0}; // shiftign register
+        i2s_data <= {i2s_data[14:0], 1'b0}; // shiftign register
     end
 end
 
 assign dac_bck = ym_dclk;
-assign dac_dat = i2s_data[15];
+assign dac_dat = (cfg[0]) ? ym_data : i2s_data[15];
+assign dac_std = cfg[0]; // 0 - i2s, 1 - lsb first
+assign led[0] = cfg[0];
+assign led[1] = ym_clk_cnt[15];
 
-// another idea:
+// todo: another idea to try:
 // 1. sample ym_dclk, ym_smp, ym_data with the clk28
 // 2. generate new clock from clk28/18 as dac_bck
 // 3. generate dac_lrck, dac_dat from the logic above
--- cpld/syn/clocks.sdc
+++ cpld/syn/clocks.sdc
@@ -1,5 +1,5 @@
 create_clock -period 28.6MHz -name {clk_28mhz} [get_ports {clk28}]
 
-create_generated_clock -name {clk14} -divide_by 2 -source [get_ports {clk28}] [get_registers {clk14_cnt[0]}]
+create_generated_clock -name {clk14} -divide_by 2 -source [get_ports {clk28}] [get_registers {clk_div}]
 
 set_false_path -from [get_ports {cfg[*]}]
